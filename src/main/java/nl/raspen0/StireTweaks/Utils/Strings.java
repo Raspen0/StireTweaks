eb1f012ec67a3ce057daa50c921bd34fa4d03bce
Raspen0 <raspen0@users.noreply.github.com>
2015-08-28 13:48:37 +0200

    Update version to 2.1.2 (Always forget to update it)

--- src/main/java/nl/raspen0/StireTweaks/Utils/Strings.java
+++ src/main/java/nl/raspen0/StireTweaks/Utils/Strings.java
@@ -7,7 +7,7 @@ public class Strings {
 
 	public static final String MODID = "StireTweaks";
 	public static final String name = "StireTweaks";
-	public static final String VERSION = "2.1.1";
+	public static final String VERSION = "2.1.2";
 	public static final String PackVersion = ConfigHandler.PackVersion;
 	public static final Logger logger = LogManager.getLogger(name);
 
